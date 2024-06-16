476647efa8c650da043661ad442d2415074867a4
Fernando Ayats <ayatsfer@gmail.com>
2024-06-16 11:35:18 +0200

    read files

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <math.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <byteswap.h>
 
 #include "matrix.h"
 
@@ -15,10 +17,33 @@ double sig(double x) {
   return exponent / (1 + exponent);
 }
 
+/*
+http://yann.lecun.com/exdb/mnist/
+*/
+
 int main() {
   Matrix* m = m_init(3, 3);
   m_print(m);
   m_delete(m);
+
+  FILE* file = fopen("../data/train-images-idx3-ubyte", "r");
+
+  int32_t buf = {};
+  fread(&buf, sizeof(int32_t), 1, file);
+  buf = bswap_32(buf);
+  printf("magic=0x%x\n", buf);
+
+  fread(&buf, sizeof(int32_t), 1, file);
+  buf = bswap_32(buf);
+  printf("images=%d\n", buf);
+
+  fread(&buf, sizeof(int32_t), 1, file);
+  buf = bswap_32(buf);
+  printf("rows=%d\n", buf);
+
+  fread(&buf, sizeof(int32_t), 1, file);
+  buf = bswap_32(buf);
+  printf("columns=%d\n", buf);
 
   return EXIT_SUCCESS;
 }
